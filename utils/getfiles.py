0d7c96184dc714af50d2d3a0a85f4fad39a310cc
sajeev-68 <133692484+sajeev-68@users.noreply.github.com>
2024-12-24 17:31:52 -0500

    Update getfiles.py
    directory

--- utils/getfiles.py
+++ utils/getfiles.py
@@ -2,7 +2,7 @@ import os
 
 def getnewfiles_as_list():
     current_file = "files.txt"
-    directory_path = '/home/sssajeev682/tpotce/data/dionaea/binaries'
+    directory_path = '/home/<ur_directory_here>/tpotce/data/dionaea/binaries' #change directory name to reflect your system
     now_binaries = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
     with open(current_file, "r") as file:
         old_binaries = [line.strip() for line in file]
